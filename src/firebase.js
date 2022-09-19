e5a50af907e4b1bc031d5e355513c2cdb43143c4
APURVA SINGH <singhapurva42@gmail.com>
2022-09-19 22:45:30 +0530

    Setting up React

---
+++ src/firebase.js
@@ -0,0 +1,8 @@
+const firebaseConfig = {
+    apiKey: "AIzaSyAtjhJJD3--JB_GFg-Su4ygPDInLruP1cw",
+    authDomain: "whatsapp-mern-7a0ed.firebaseapp.com",
+    projectId: "whatsapp-mern-7a0ed",
+    storageBucket: "whatsapp-mern-7a0ed.appspot.com",
+    messagingSenderId: "935738274421",
+    appId: "1:935738274421:web:5a8fd3e6f30ca2373b3e3c"
+  };
